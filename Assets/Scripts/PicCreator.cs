bdd5ac217fb8b63a926185f2164b630bde3bf5ed
XENKing <XENKings@gmail.com>
2018-03-22 23:55:27 +0700

    Try to create pixel drawer
    lulz

---
+++ Assets/Scripts/PicCreator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PicCreator : MonoBehaviour {
+
+    private Color color;
+    
+    public void SetCurrentColor(Image image)
+    {
+        color = image.color;
+        Debug.Log(color);
+    }
+
+    public void Paint(Image pixel)
+    {
+        pixel.color = color;
+        Debug.Log(this);
+    }
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Pixel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Pixel : MonoBehaviour, IPointerEnterHandler
+{
+
+    public PicCreator picCreator;
+
+	// Use this for initialization
+	void Awake () {
+        picCreator = FindObjectOfType<PicCreator>();
+    }
+	
+	// Update is called once per frame
+	public void OnPointerEnter(PointerEventData eventData) {
+        Debug.Log(eventData.clickCount);
+        if (eventData.clickCount == 2)
+        {
+            picCreator.Paint(GetComponent<Image>());
+            Debug.Log("hui1");
+        }
+    }
+
+}
